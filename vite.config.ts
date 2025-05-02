ca5696a1d73516f131747110db80f9789e9eb28e
samuellimabraz <samuellimabraz@gmail.com>
2025-05-02 03:52:42 -0300

    Update index.html to improve icon formatting, change debug script to module type, and update package.json and package-lock.json to include Terser as a dependency. Modify GitHub Actions workflow for better deployment process and artifact handling.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Mudando para caminho relativo para GitHub Pages
+  base: './', // Relative path for GitHub Pages
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
